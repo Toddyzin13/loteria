f32b53cf11b758d3faba4cdaf0b19bf6c409e859
Toddyzin13 <99762737+Toddyzin13@users.noreply.github.com>
2022-03-08 18:29:41 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -15,20 +15,32 @@ function getnum(valor, posicao){
     valor=Number(valor)
 if(valor > 60){
     alert("valor é maior que 60")
+    limpar(pos)
 }
 else if(valor<0){
 alert("valor menor que 0")
+limpar(pos)
 }
 else if(numsco. includes(valor)){
 alert("valor já digitado")
+limpar(pos)
 }
 else{
     numsco(posicao)=valor
 }}
+function limpar(pos){
+document.getElementById(`num${pos}`).value = ""
+}
 function verificacertos(){
     numsco.forEach(function(valor,index){
         if(numSort.includes(valor)){
     cont+ cont+1
+    }
+})
+document.getElementById("total").innerText = cont
+
+}
+
     }
 })
 document.getElementById("total").innerText = cont
